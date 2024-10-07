4f6840d177153f621b94bd1cb9a21ccdaa05b92a
zode <zhangdi.zode@bytedance.com>
2024-10-07 12:07:57 +0800

    add options

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -7,4 +7,7 @@ option(ZOM_ENABLE_UNDEFINED_SANITIZER
 option(ZOM_ENABLE_WERROR "Treat warnings as errors (-Werror)" ON)
 option(ZOM_ENABLE_WALL "Enable most warnings (-Wall -Wextra)" ON)
 option(ZOM_DISABLE_GLOB_CTOR
-       "Disable declare global or static variables with dynamic constructors" ON)
+       "Disable declare global or static variables with dynamic constructors"
+       ON)
+option(BUILD_STATIC_LIB "Build ZOM as a static library" ON)
+option(BUILD_CLI "Build ZOM CLI" ON)
